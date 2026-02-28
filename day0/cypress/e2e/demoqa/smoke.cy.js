badb246bdcaee8bebab1d293b84a13639918c568
MaksimsALV <maxim.ahmetov@gmail.com>
2026-03-01 00:26:41 +0200

    fixing smoke.cy.js

--- day0/cypress/e2e/demoqa/smoke.cy.js
+++ day0/cypress/e2e/demoqa/smoke.cy.js
@@ -3,17 +3,18 @@ describe('Demo QA - ViA - Maksims Ahmetovs', () => {
         beforeEach(() => {
             cy.visit('/text-box');
         });
-    })
-    it('Set text box values', () => {
-        // set Full Name
-        cy.get('#userName').type('Maksims Ahmetovs');
-        // set Email
-        cy.get('#userName').type('maksims.ahmetovs@va.lv');
-        // set address
-        cy.get('textarea#currentAddress').type('My current address');
-        // set permament address
-        cy.get('textarea#permamentAddress').type('My permanent address');
-        // click submit pogu
-        cy.get('#submit').click();
-    })
-})
+
+        it('Set text box values', () => {
+            // set Full Name
+            cy.get('#userName').type('Maksims Ahmetovs');
+            // set Email
+            cy.get('#userName').type('maksims.ahmetovs@va.lv');
+            // set address
+            cy.get('textarea#currentAddress').type('My current address');
+            // set permament address
+            cy.get('textarea#permamentAddress').type('My permanent address');
+            // click submit pogu
+            cy.get('#submit').click();
+        });
+    });
+});
